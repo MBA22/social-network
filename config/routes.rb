5fd77f27b7322313ecf38869b4cdf0ed89ce9ac3
Mario Barragán <mario.barragan.2012@gmail.com>
2015-05-11 12:03:37 -0500

    Add a partial show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users
  # get 'welcome/index'
   resources :twits
   # The priority is based upon order of creation: first created -> highest priority.
